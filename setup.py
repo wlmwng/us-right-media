942b6315ba6cf8b1bba06008eef921bc0b89366c
Wai Lam <23304546+wlmwng@users.noreply.github.com>
2021-11-24 02:50:05 +0000

    make repo installable as a package
    allows functions to be shared across notebooks

---
+++ setup.py
@@ -0,0 +1,5 @@
+# https://stackoverflow.com/questions/714063/importing-modules-from-parent-folder
+
+from setuptools import setup, find_packages
+
+setup(name="usrightmedia", version="1.0", packages=["usrightmedia.shared"])
